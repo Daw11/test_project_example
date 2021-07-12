4d77bf0d93b374d92782ffd8cfa9a777f247f624
davide mastroianni <davide.mastroianni@euris.it>
2021-07-12 17:46:52 +0200

    calculator.add basic test

---
+++ src/test/java/it/euris/ires/CalculatorTests.java
@@ -0,0 +1,19 @@
+package it.euris.ires;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalculatorTests {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+
+}
