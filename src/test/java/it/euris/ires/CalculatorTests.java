f86b913cb6e2a8fedb7cdc43c79f858065238ee9
davide mastroianni <davide.mastroianni@euris.it>
2021-07-12 18:09:49 +0200

    arrange-act-assert pattern

--- src/test/java/it/euris/ires/CalculatorTests.java
+++ src/test/java/it/euris/ires/CalculatorTests.java
@@ -10,15 +10,31 @@ class CalculatorTests {
 	@Test
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
+		// arrange
+		int numberA = 1;
+		int numberB = 1;
+		int expectedResult = 2;
 		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+		// act
+		int result = calculator.add(numberA, numberB);
+		// assert
+		assertEquals(expectedResult, result,
+				String.format("%s + %s should equal %s", numberA, numberB, expectedResult));
 	}
 
 	@Test
 	@DisplayName("1 - 1 = 0")
 	void subtractTwoNumbers() {
+		// arrange
+		int numberA = 1;
+		int numberB = 1;
+		int expectedResult = 0;
 		Calculator calculator = new Calculator();
-		assertEquals(0, calculator.subtract(1, 1), "1 + 1 should equal 0");
+		// act
+		int result = calculator.subtract(numberA, numberB);
+		// assert
+		assertEquals(expectedResult, result,
+				String.format("%s + %s should equal %s", numberA, numberB, expectedResult));
 	}
 
 }
