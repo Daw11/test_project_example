76f047f626ff8c16f557f77cee4cffc2dda08215
davide mastroianni <davide.mastroianni@euris.it>
2021-07-13 19:35:25 +0200

    test doubles - mock

--- src/test/java/it/euris/ires/testDoubles/PaymentServiceTest.java
+++ src/test/java/it/euris/ires/testDoubles/PaymentServiceTest.java
@@ -11,11 +11,16 @@ public class PaymentServiceTest {
 
   PaymentService paymentService;
 
+  ILogger logger;
+  IOperatorRate operatorRate;
+  PaymentEmailSenderMock emailSender;
+
   @BeforeEach
   void setUp() {
-    ILogger logger = new LoggerDummy();
-    IOperatorRate operatorRate = new OperatorRateStub(10);
-    paymentService = new PaymentService(logger, operatorRate);
+    logger = new LoggerDummy();
+    operatorRate = new OperatorRateStub(10);
+    emailSender = new PaymentEmailSenderMock();
+    paymentService = new PaymentService(logger, operatorRate, emailSender);
   }
 
   @Test
@@ -26,11 +31,28 @@ public class PaymentServiceTest {
     items.add(item);
     Sale sale = new Sale(customer, items);
     CreditCard creditCard = new CreditCard(customer, "1");
-    PaymentRequest expectedResult = new PaymentRequest(1000, "1", 10);
+    PaymentRequest expectedResult = new PaymentRequest(1000, "1", 100);
 
     PaymentRequest result = paymentService.createPaymentRequest(sale, creditCard);
 
     assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  void givenSaleOver1000AndCreditCardWhenCreatePaymentRequestThenReturnPaymentRequestThenSendMail() {
+    Customer customer= new Customer("name", "address");
+    Item item = new Item("item", 1001);
+    List<Item> items = new ArrayList<>();
+    items.add(item);
+    Sale sale = new Sale(customer, items);
+    CreditCard creditCard = new CreditCard(customer, "1");
+    PaymentRequest expectedResult = new PaymentRequest(1001, "1", 100);
+    emailSender.expect(expectedResult);
+
+    PaymentRequest result = paymentService.createPaymentRequest(sale, creditCard);
+
+    assertThat(result).isEqualTo(expectedResult);
+    emailSender.verify();
   }
 
 }
